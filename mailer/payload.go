9964c2f59fd4a29d66cd3b0026b49adba6e3204f
Leon Oldfritter <leon.zcf@gmail.com>
2020-05-27 18:48:26 +0800

    临时的邮件国际化文件

--- mailer/payload.go
+++ mailer/payload.go
@@ -3,10 +3,8 @@ package mailer
 import (
 	"fmt"
 	"html/template"
-	"strings"
 
-	"github.com/spf13/viper"
-
+	"cherry/config"
 	. "cherry/models"
 )
 
@@ -19,90 +17,6 @@ type Payload struct {
 	FuncMap template.FuncMap `json:"-"`
 }
 
-func (mp *Payload) I18nFuncName() string {
-	arr := strings.Split(mp.Method, "_")
-	for i, ar := range arr {
-		arr[i] = strings.Title(ar)
-	}
-	return strings.Join(arr, "")
-}
-
-func (mp *Payload) Activation() {
-	mp.Subject = fmt.Sprint(I18n.T(mp.Locale, "token_mailer.activation.subject"))
-	mp.Emails = []string{mp.Args[0]}
-	mp.FuncMap = template.FuncMap{
-		"title": func() string {
-			return fmt.Sprint(I18n.T(mp.Locale, "token_mailer.activation.title"))
-		},
-		"content": func() string {
-			return fmt.Sprint(I18n.T(mp.Locale, "token_mailer.activation.content", map[string]interface{}{"link": mp.Args[1]}))
-		},
-		"foot": func() template.HTML {
-			return template.HTML(fmt.Sprint(I18n.T(mp.Locale, "mailer.footer", map[string]interface{}{"contact_mail": viper.GetString("email.support_email")})))
-		},
-	}
-}
-
-func (mp *Payload) AppResetPassword() {
-	mp.Subject = fmt.Sprint(I18n.T(mp.Locale, "token_mailer.app_reset_password.subject"))
-	mp.Emails = []string{mp.Args[0]}
-	mp.FuncMap = template.FuncMap{
-		"title": func() string {
-			return fmt.Sprint(I18n.T(mp.Locale, "token_mailer.app_reset_password.title", map[string]interface{}{"email": mp.Emails[0]}))
-		},
-		"content": func() string {
-			return fmt.Sprint(I18n.T(mp.Locale, "token_mailer.app_reset_password.content", map[string]interface{}{"token": mp.Args[1]}))
-		},
-		"foot": func() template.HTML {
-			return template.HTML(fmt.Sprint(I18n.T(mp.Locale, "mailer.footer", map[string]interface{}{"contact_mail": viper.GetString("email.support_email")})))
-		},
-	}
-}
-
-func (mp *Payload) SetFundPassword() {
-	mp.Subject = fmt.Sprint(I18n.T(mp.Locale, "token_mailer.set_fund_password.subject"))
-	mp.Emails = []string{mp.Args[0]}
-	mp.FuncMap = template.FuncMap{
-		"title": func() string {
-			return fmt.Sprint(I18n.T(mp.Locale, "token_mailer.set_fund_password.title", map[string]interface{}{"email": mp.Emails[0]}))
-		},
-		"content": func() string {
-			return fmt.Sprint(I18n.T(mp.Locale, "token_mailer.set_fund_password.content", map[string]interface{}{"token": mp.Args[1]}))
-		},
-		"foot": func() template.HTML {
-			return template.HTML(fmt.Sprint(I18n.T(mp.Locale, "mailer.footer", map[string]interface{}{"contact_mail": viper.GetString("email.support_email")})))
-		},
-	}
-}
-
-func (mp *Payload) BindEmail() {
-	mp.Subject = fmt.Sprint(I18n.T(mp.Locale, "token_mailer.bind_email.subject"))
-	mp.Emails = []string{mp.Args[0]}
-	mp.FuncMap = template.FuncMap{
-		"title": func() string {
-			return fmt.Sprint(I18n.T(mp.Locale, "token_mailer.bind_email.title", map[string]interface{}{"email": mp.Emails[0]}))
-		},
-		"content": func() string {
-			return fmt.Sprint(I18n.T(mp.Locale, "token_mailer.bind_email.content", map[string]interface{}{"token": mp.Args[1]}))
-		},
-		"foot": func() template.HTML {
-			return template.HTML(fmt.Sprint(I18n.T(mp.Locale, "mailer.footer", map[string]interface{}{"contact_mail": viper.GetString("email.support_email")})))
-		},
-	}
-}
-
-func (mp *Payload) EmailCodeVerified() {
-	mp.Subject = fmt.Sprint(I18n.T(mp.Locale, "token_mailer.email_code_verified.subject"))
-	mp.Emails = []string{mp.Args[0]}
-	mp.FuncMap = template.FuncMap{
-		"title": func() string {
-			return fmt.Sprint(I18n.T(mp.Locale, "token_mailer.email_code_verified.title", map[string]interface{}{"email": mp.Emails[0]}))
-		},
-		"content": func() string {
-			return fmt.Sprint(I18n.T(mp.Locale, "token_mailer.email_code_verified.content", map[string]interface{}{"token": mp.Args[1]}))
-		},
-		"foot": func() template.HTML {
-			return template.HTML(fmt.Sprint(I18n.T(mp.Locale, "mailer.footer", map[string]interface{}{"contact_mail": viper.GetString("email.support_email")})))
-		},
-	}
+func (mp *Payload) foot() template.HTML {
+	return template.HTML(fmt.Sprint(I18n.T(mp.Locale, "mailer.footer", map[string]interface{}{"contact_mail": config.CurrentEnv.Email.SupportMail})))
 }
---
+++ mailer/tokenMailer.go
@@ -0,0 +1,90 @@
+package mailer
+
+import (
+	"fmt"
+	"html/template"
+	"strings"
+
+	"cherry/config"
+	. "cherry/models"
+)
+
+func (mp *Payload) I18nFuncName() string {
+	arr := strings.Split(mp.Method, "_")
+	for i, ar := range arr {
+		arr[i] = strings.Title(ar)
+	}
+	return strings.Join(arr, "")
+}
+
+func (mp *Payload) Activation() {
+	mp.Subject = fmt.Sprint(I18n.T(mp.Locale, "token_mailer.activation.subject"))
+	mp.Emails = []string{mp.Args[0]}
+	mp.FuncMap = template.FuncMap{
+		"title": func() string {
+			return fmt.Sprint(I18n.T(mp.Locale, "token_mailer.activation.title"))
+		},
+		"content": func() string {
+			return fmt.Sprint(I18n.T(mp.Locale, "token_mailer.activation.content", map[string]interface{}{"link": mp.Args[1]}))
+		},
+		"foot": func() template.HTML {
+			return template.HTML(fmt.Sprint(I18n.T(mp.Locale, "mailer.footer", map[string]interface{}{"contact_mail": config.CurrentEnv.Email.SupportMail})))
+		},
+	}
+}
+
+func (mp *Payload) AppResetPassword() {
+	mp.Subject = fmt.Sprint(I18n.T(mp.Locale, "token_mailer.app_reset_password.subject"))
+	mp.Emails = []string{mp.Args[0]}
+	mp.FuncMap = template.FuncMap{
+		"title": func() string {
+			return fmt.Sprint(I18n.T(mp.Locale, "token_mailer.app_reset_password.title", map[string]interface{}{"email": mp.Emails[0]}))
+		},
+		"content": func() string {
+			return fmt.Sprint(I18n.T(mp.Locale, "token_mailer.app_reset_password.content", map[string]interface{}{"token": mp.Args[1]}))
+		},
+		"foot": mp.foot,
+	}
+}
+
+func (mp *Payload) SetFundPassword() {
+	mp.Subject = fmt.Sprint(I18n.T(mp.Locale, "token_mailer.set_fund_password.subject"))
+	mp.Emails = []string{mp.Args[0]}
+	mp.FuncMap = template.FuncMap{
+		"title": func() string {
+			return fmt.Sprint(I18n.T(mp.Locale, "token_mailer.set_fund_password.title", map[string]interface{}{"email": mp.Emails[0]}))
+		},
+		"content": func() string {
+			return fmt.Sprint(I18n.T(mp.Locale, "token_mailer.set_fund_password.content", map[string]interface{}{"token": mp.Args[1]}))
+		},
+		"foot": mp.foot,
+	}
+}
+
+func (mp *Payload) BindEmail() {
+	mp.Subject = fmt.Sprint(I18n.T(mp.Locale, "token_mailer.bind_email.subject"))
+	mp.Emails = []string{mp.Args[0]}
+	mp.FuncMap = template.FuncMap{
+		"title": func() string {
+			return fmt.Sprint(I18n.T(mp.Locale, "token_mailer.bind_email.title", map[string]interface{}{"email": mp.Emails[0]}))
+		},
+		"content": func() string {
+			return fmt.Sprint(I18n.T(mp.Locale, "token_mailer.bind_email.content", map[string]interface{}{"token": mp.Args[1]}))
+		},
+		"foot": mp.foot,
+	}
+}
+
+func (mp *Payload) EmailCodeVerified() {
+	mp.Subject = fmt.Sprint(I18n.T(mp.Locale, "token_mailer.email_code_verified.subject"))
+	mp.Emails = []string{mp.Args[0]}
+	mp.FuncMap = template.FuncMap{
+		"title": func() string {
+			return fmt.Sprint(I18n.T(mp.Locale, "token_mailer.email_code_verified.title", map[string]interface{}{"email": mp.Emails[0]}))
+		},
+		"content": func() string {
+			return fmt.Sprint(I18n.T(mp.Locale, "token_mailer.email_code_verified.content", map[string]interface{}{"token": mp.Args[1]}))
+		},
+		"foot": mp.foot,
+	}
+}
